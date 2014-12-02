3e7ffdecc49a52b7cc4ff525f529e50939651b24
Elias Lindholm <elias.lindholm@avanza.se>
2014-12-02 12:32:20 +0100

    #55 Fixed bug in GsBinder parsing spaceUrls

---
+++ astrix-gs/src/test/java/com/avanza/astrix/gs/GsBinderTest.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2014-2015 Avanza Bank AB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.avanza.astrix.gs;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.avanza.astrix.context.AstrixServiceProperties;
+
+
+
+public class GsBinderTest {
+	
+	@Test
+	public void parsesASpaceUrl() throws Exception {
+		AstrixServiceProperties serviceProperties = GsBinder.createServiceProperties("jini://*/*/service-registry-space?locators=testgssystem01.test.aza.se,testgssystem02.test.aza.se");
+		assertEquals("service-registry-space", serviceProperties.getProperty(GsBinder.SPACE_NAME_PROPERTY));
+		assertEquals("service-registry-space", serviceProperties.getQualifier());
+		assertEquals("jini://*/*/service-registry-space?locators=testgssystem01.test.aza.se,testgssystem02.test.aza.se", serviceProperties.getProperty(GsBinder.SPACE_URL_PROPERTY));
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void throwsIllegalArgumentExceptionForInvalidSpaceUrls() throws Exception {
+		GsBinder.createServiceProperties("jini://service-registry-space?locators=testgssystem01.test.aza.se,testgssystem02.test.aza.se");
+	}
+
+}
