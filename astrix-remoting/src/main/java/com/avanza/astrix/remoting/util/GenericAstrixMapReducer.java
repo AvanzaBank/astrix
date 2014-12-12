04b31acb43ebdce278273e4c7c1a1a04b49f24a0
Joakim Sahlstrom <joakim.sahlstrom@avanza.se>
2014-12-12 08:26:02 +0100

    Added generic reducers for map-reduce for List and Map

---
+++ astrix-remoting/src/main/java/com/avanza/astrix/remoting/util/GenericAstrixMapReducer.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2014-2015 Avanza Bank AB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.avanza.astrix.remoting.util;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.avanza.astrix.core.AstrixRemoteResult;
+import com.avanza.astrix.core.AstrixRemoteResultReducer;
+
+public class GenericAstrixMapReducer<K, V> implements AstrixRemoteResultReducer<Map<K, V>, Map<K, V>> {
+
+	@Override
+	public Map<K, V> reduce(List<AstrixRemoteResult<Map<K, V>>> results) {
+		Map<K, V> map = new HashMap<K, V>();
+		for (AstrixRemoteResult<Map<K, V>> result : results) {
+			map.putAll(result.getResult());
+		}
+		return map;
+	}
+
+}
