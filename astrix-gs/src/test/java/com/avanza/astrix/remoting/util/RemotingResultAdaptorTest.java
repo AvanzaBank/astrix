b27de8d22661f746374d042a41847089eb91630e
Joakim Sahlstrom <joakim.sahlstrom@avanza.se>
2014-12-12 12:50:00 +0100

    Added test case implementations

--- astrix-gs/src/test/java/com/avanza/astrix/remoting/util/RemotingResultAdaptorTest.java
+++ astrix-gs/src/test/java/com/avanza/astrix/remoting/util/RemotingResultAdaptorTest.java
@@ -17,13 +17,74 @@ package com.avanza.astrix.remoting.util;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 import org.junit.Test;
+import org.openspaces.remoting.SpaceRemotingResult;
+
+import com.avanza.astrix.core.AstrixRemoteResult;
 
 public class RemotingResultAdaptorTest {
 
 	@Test
-	public void testName() throws Exception {
-		
+	public void getResult_astrixRemotingResultWrap() throws Throwable {
+		assertEquals("hej", 
+				RemotingResultAdaptor.AstrixRemotingResultWrap.wrap(Arrays.asList(new AstrixRemoteResult<>("hej", null))).iterator().next().getResult());
+	}
+	
+	@Test
+	public void getResult_gigaSpaceRemotingResultWrap() throws Throwable {
+		@SuppressWarnings("unchecked")
+		Collection<RemotingResultAdaptor<?>> wrap = (Collection<RemotingResultAdaptor<?>>) 
+			RemotingResultAdaptor.GigaSpaceRemotingResultWrap.wrap(new SpaceRemotingResult[] { new MySpaceRemotingResult<String>("hej", null) });
+		assertEquals("hej", wrap.iterator().next().getResult());
+	}
+	
+	@Test(expected = MyException.class)
+	public void getResult_astrixRemotingResultWrap_rethrowsException() throws Throwable {
+		RemotingResultAdaptor.AstrixRemotingResultWrap.wrap(Arrays.asList(new AstrixRemoteResult<>(null, new MyException()))).iterator().next().getResult();
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Test(expected = MyException.class)
+	public void getResult_gigaSpaceRemotingResultWrap_rethrowsException() throws Throwable {
+		((Collection<RemotingResultAdaptor<?>>) RemotingResultAdaptor.GigaSpaceRemotingResultWrap.wrap(new SpaceRemotingResult[] { new MySpaceRemotingResult<String>(null, new MyException()) }))
+			.iterator().next().getResult();
+	}
+	
+	public static class MySpaceRemotingResult<T> implements SpaceRemotingResult<T> {
+		private T result;
+		private Throwable exception;
+
+		public MySpaceRemotingResult(T result, Throwable exception) {
+			this.result = result;
+			this.exception = exception;
+		}
+
+		@Override
+		public Integer getRouting() {
+			return 1;
+		}
+
+		@Override
+		public T getResult() {
+			return result;
+		}
+
+		@Override
+		public Throwable getException() {
+			return exception;
+		}
+
+		@Override
+		public Integer getInstanceId() {
+			return null;
+		}
+	}
+	
+	public static class MyException extends RuntimeException {
+		private static final long serialVersionUID = 1L;
 	}
 	
 }
