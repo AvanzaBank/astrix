ad33b696eeb29c8357ee75df386393280a78540b
Elias Lindholm <elias.lindholm@avanza.se>
2015-01-20 17:56:48 +0100

    Updated doc

--- astrix-provider/src/main/java/com/avanza/astrix/provider/core/AstrixApiProvider.java
+++ astrix-provider/src/main/java/com/avanza/astrix/provider/core/AstrixApiProvider.java
@@ -28,31 +28,34 @@ import java.lang.annotation.Target;
  * An ApiProvider is used to define all Astrix-beans that is part of a given api, and
  * to provide information about how to bind to an instance of a each bean in the api at runtime. 
  * 
- * Astrix defines two distinct types of beans: "libraries" and "services". The difference 
- * between a "library" and a "service" is how they are bound at runtime. For libraries, the
- * class holding this annotation is responsible for binding, i.e it should contain a method
- * that can be used as a factory to create ("bind to") a given bean exported in the api. Such
- * a method should be annotated with @Library indicating:
+ * Astrix defines two distinct types of beans: "libraries" and "services". Simply put, a "service"
+ * can be seen of as a bean that uses some kind of remote-service-invocation to invoke a provider of
+ * a given instance, whereas a "library" uses simple in-memory method calls.
+ * 
+ * A more formal distinction between a "library" and a "service" is how they are bound at runtime. 
+ * For libraries, the class holding this annotation should define a factory method for each exported library bean in
+ * the api. At runtime when Astrix binds to a provider of a given bean type by invoking the corresponding factory 
+ * method and use the returned instance as provider. Such a method should be annotated with @Library indicating:
  * 
  * 1. That this api provides a bean of the type returned by the method
  * 2. That the given method should be used as a factory to bind to a provider of the given bean type
  * 
- * The other type of bean is "service". For services, the Astrix runtime is responsible for binding 
- * to a given provider instance. This binding is done using an AstrixServiceComponent, which
- * is the extension point to define custom service transports. A service is defined by
- * annotating a method with @Library indicating:
+ * The other type of bean is "service". For services Astrix uses a AstrixServiceComponent to bind to a provider
+ * of the given bean type. The AstrixServiceComponent is an extension point to create custom service transports.
+ * A service is defined by annotating a method with @Servcie indicating:
  * 
  * 1. That this api provides a service of the type returned by the method
- * 2. Optional more information about how to "lookup" the given service, by default Astrix will
+ * 2. Optional information about how to "lookup" the given service, by default Astrix will
  * lookup a given service using the service-registry.
  * 
  * Libraries are sometimes referred to as "statically bound Astrix beans" reflecting that 
  * the Astrix bean instance can't change at runtime. Accordingly, services are sometimes referred to
  * as "dynamically bound Astrix beans", reflecting that they can be rebound at runtime. For
- * instance to a completely new provider using another transport mechanism (as defined by
+ * instance, when a new provider for a given service is registered in the service registry, 
+ * b to a completely new provider using another transport mechanism (as defined by
  * the corresponding AstrixServiceComponent) if one shows up in the service registry.  
  * 
- * Example service provider exporting two services that are lookup up on the service registry:
+ * Example of a service provider exporting two services that are located using the service registry:
  * 
  * <pre>
  * @AstrixApiProvider
@@ -66,7 +69,7 @@ import java.lang.annotation.Target;
  * </pre>
  * 
  * 
- * Example library provider exporting a single library astrix bean. 
+ * Example library provider exporting a single library Astrix bean. 
  * 
  * <pre>
  * @AstrixApiProvider
