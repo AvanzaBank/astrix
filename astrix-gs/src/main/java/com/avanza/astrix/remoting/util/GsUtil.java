bd644dc384fe6c8a6a5dfa7e9af737dc8cbc542d
Elias Lindholm <elias.lindholm@avanza.se>
2015-05-15 11:20:26 +0200

    Restored removed method from GsUtil

--- astrix-gs/src/main/java/com/avanza/astrix/remoting/util/GsUtil.java
+++ astrix-gs/src/main/java/com/avanza/astrix/remoting/util/GsUtil.java
@@ -15,13 +15,20 @@
  */
 package com.avanza.astrix.remoting.util;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import org.openspaces.remoting.SpaceRemotingResult;
 
 import rx.Observable;
 import rx.Observable.OnSubscribe;
 import rx.Subscriber;
 import rx.functions.Func1;
 
+import com.avanza.astrix.core.AstrixRemoteResult;
+import com.avanza.astrix.core.CorrelationId;
+import com.avanza.astrix.core.RemoteServiceInvocationException;
+import com.avanza.astrix.core.ServiceInvocationException;
 import com.gigaspaces.async.AsyncFuture;
 import com.gigaspaces.async.AsyncFutureListener;
 import com.gigaspaces.async.AsyncResult;
@@ -30,6 +37,27 @@ import com.gigaspaces.async.AsyncResult;
  * @author joasah
  */
 public class GsUtil {
+	
+	@Deprecated
+	public static <T> List<AstrixRemoteResult<T>> convertToAstrixRemoteResults(SpaceRemotingResult<T>[] results) {
+		List<AstrixRemoteResult<T>> converted = new ArrayList<AstrixRemoteResult<T>>(results.length);
+		for (SpaceRemotingResult<T> result : results) {
+			if (result.getException() != null) {
+				converted.add(AstrixRemoteResult.<T>failure(toRuntimeException(result.getException()), CorrelationId.undefined()));
+			} else {
+				converted.add(AstrixRemoteResult.successful(result.getResult()));
+			}
+		}
+		return converted;
+	}
+	
+	private static ServiceInvocationException toRuntimeException(Throwable exception) {
+		if (exception instanceof RuntimeException) {
+			return (ServiceInvocationException) exception;
+		} else {
+			return new RemoteServiceInvocationException("Remote service threw exception: " + exception.getMessage(), exception.getClass().getName(), ServiceInvocationException.UNDEFINED_CORRELATION_ID);
+		}
+	}
 	
 	public static <T> Observable<T> toObservable(final AsyncFuture<T> response) {
 		return Observable.create(new Observable.OnSubscribe<T>() {
