7fb81a89d2ab07d33e63d81430f98ca8ec22e405
Elias Lindholm <elias.lindholm@avanza.se>
2015-05-20 14:14:38 +0200

    Updated release.bat

--- release.bat
+++ release.bat
@@ -1,6 +1,6 @@
 
 call git checkout -b %1
-call versions:set -DnewVersion=%1 -DgenerateBackupPoms=false
+call mvn versions:set -DnewVersion=%1 -DgenerateBackupPoms=false
 call git commit -am "Release %1"
 call git tag -a %1
 call git checkout master
