93371bef45c8efdb1c0192430235b480d307f32f
Elias Lindholm <elias.lindholm@avanza.se>
2015-05-20 14:10:18 +0200

    Update release script

--- release.bat
+++ release.bat
@@ -1,8 +1,8 @@
 
-echo "git checkout -b %1"
-echo "mvn versions:set -DnewVersion=%1 -DgenerateBackupPoms=false"
-echo "git commit -am "Release %1"
-echo "git tag -a %1"
+call git checkout -b %1
+call mvn versions:set -DnewVersion=%1 -DgenerateBackupPoms=false
+call git commit -am "Release %1"
+call git tag -a %1
 
 echo "git push --tags origin heads/%1"
 
