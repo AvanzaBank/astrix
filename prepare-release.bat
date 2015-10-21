e32041dc3b72160f6db37f1fbe356dd7b62ade7c
Elias Lindholm <elias.lindholm@avanza.se>
2015-10-21 16:42:33 +0200

    Preparations for publish to maven central

--- prepare-release.bat
+++ prepare-release.bat
@@ -2,4 +2,6 @@ call git checkout -b %1
 call mvn versions:set -DnewVersion=%1 -DgenerateBackupPoms=false
 call git commit -am "Release %1"
 call git tag -a %1
+call git checkout refs/tags/%1
+call git branch -D %1
 
