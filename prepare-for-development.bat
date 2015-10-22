a4d2a7bc6958c92465a336045c4d866e08b11814
Elias Lindholm <elias.lindholm@avanza.se>
2015-10-22 09:09:47 +0200

    Updates bat script to prepare for next development version

--- prepare-for-development.bat
+++ prepare-for-development.bat
@@ -1,2 +1,15 @@
+@echo off
+if [%1]==[] goto usage
 call git checkout master
 call mvn versions:set -DnewVersion=%1-SNAPSHOT -DgenerateBackupPoms=false
+call git commit -am "Prepare for development of %1-SNAPSHOT" 
+@echo.
+@echo "Done: Current development version is %1-SNAPSHOT" 
+goto :eof
+
+
+:usage
+@echo Usage: prepare-for-development.bat MAJOR.MINOR.PATCH
+@echo.
+@echo Example:
+@echo ^> prepare-for-development.bat 1.0.1
